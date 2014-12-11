40d860be1086f8fc1f708b78117a4160ed42bb58
Taylor Huston <taylor.a.huston@gmail.com>
2014-12-10 23:31:49 -0700

    Fix formatting based on checkstyle

--- Student.java
+++ Student.java
@@ -6,63 +6,63 @@
 public class Student {
     private String name;
     private double average;
-    
+
     public Student(String newName, double newAve) {
         this.name = newName;
-        
+
         //Validates that the average is between 0 and 100
-        if(newAve > 0.0 && newAve < 100) {
+        if (newAve > 0.0 && newAve < 100) {
             this.average = newAve;
         } else {
             this.average = 0.0;
         }
-    }
-    
-    //Changes the student's name
+    } //End constructor
+
+    //Getters and Setters
     public void setName(String newName) {
         this.name = newName;
-    }
-    
-    //Get's the student's name
+    } //End setName
+
     public String getName() {
         return this.name;
-    }
-    
-    //Sets the students average
-    public void setAverage(double newAve){
+    } //End getName
+
+    public void setAverage(double newAve) {
         //Validates that the average is between 0 and 100
-        if(newAve > 0.0 && newAve < 100) {
+        if (newAve > 0.0 && newAve < 100) {
             this.average = newAve;
         } else {
             this.average = 0.0;
         }
-    }
-    
-      //Determines and returns the Student's letter grade
-      public String getLetterGrade() {
-         String letterGrade = ""; // initialized to empty String
- 
-         if (average >= 90.0)       
-            letterGrade = "A";      
-         else if (average >= 80.0)  
-            letterGrade = "B";      
-         else if (average >= 70.0)  
-            letterGrade = "C";      
-         else if (average >= 60.0)  
-            letterGrade = "D";      
-         else                       
-            letterGrade = "F";      
- 
-         return letterGrade;
-      }
- 
+    } //End setAverage
+
+    //Determines and returns the Student's letter grade
+    public String getLetterGrade() {
+        String letterGrade = ""; // initialized to empty String
+
+        if (average >= 90.0) {
+            letterGrade = "A";
+        } else if (average >= 80.0) {
+            letterGrade = "B";
+        } else if (average >= 70.0) {
+            letterGrade = "C";
+        } else if (average >= 60.0) {
+            letterGrade = "D";
+        } else {
+            letterGrade = "F";
+        }
+
+        return letterGrade;
+    } //End getLetterGrade
 
     public static void main(String[] args) {
         Student student1 = new Student("Bill Clinton", 92.74);
         Student student2 = new Student("Barack Obama", 89.35);
-        
-        System.out.printf("%s's letter grade is: %s%n", student1.getName(), student1.getLetterGrade());
-        System.out.printf("%s's letter grade is: %s%n", student2.getName(), student2.getLetterGrade());
-    }
-    
-}// end class Student
+
+        System.out.printf("%s's letter grade is: %s%n", student1.getName(), 
+                student1.getLetterGrade());
+        System.out.printf("%s's letter grade is: %s%n", student2.getName(), 
+                student2.getLetterGrade());
+    } //End main
+
+} //End Student
